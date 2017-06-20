82937a60b7b0412d5e232bb4694c87c4e637dfa0
Michael Bennett <git@michaelbennett.nyc>
2017-06-19 21:58:17 -0400

    Configured static pages and set up React entry point

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  root "static_pages#root"
+
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :update, :show]
-    resource :session, only: [:create, :destroy]
+    resources :users, only: %i[create update show]
+    resource :session, only: %i[create destroy]
   end
 end
