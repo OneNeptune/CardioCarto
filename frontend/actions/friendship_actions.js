cf7d687147517c8b43b52cef7f5bcb6fee543010
Michael Bennett <git@michaelbennett.nyc>
2017-06-28 17:47:43 -0400

    Improve Redux conventions

--- frontend/actions/friendship_actions.js
+++ frontend/actions/friendship_actions.js
@@ -8,11 +8,6 @@ export const receiveFriendships = friendships => ({
   friendships
 });
 
-const standardSuccess = (dispatch) => {
-    dispatch(clearErrors());
-    return dispatch(fetchAllFriendships());
-};
-
 export const fetchAllFriendships = () => dispatch => (
   FriendUtil.fetchAllFriendships().then((friendships) => {
       dispatch(clearErrors());
@@ -23,28 +18,39 @@ export const fetchAllFriendships = () => dispatch => (
 
 export const fetchSingleFriendship = id => dispatch => (
   FriendUtil.fetchSingleFriendship(id)
-    .then(() => standardSuccess(dispatch),
-    (errors) => dispatch(receiveErrors('friendships', errors))
+    .then(() => {
+        dispatch(clearErrors());
+        return dispatch(fetchAllFriendships());
+      }, (errors) => dispatch(receiveErrors('friendships', errors))
     )
 );
 
 export const createFriendship = friendship => dispatch => (
   FriendUtil.createFriendship(friendship)
-    .then(() => standardSuccess(dispatch),
-    (errors) => dispatch(receiveErrors('friendships', errors))
+    .then((success) => {
+        dispatch(clearErrors());
+        return dispatch(fetchAllFriendships());
+      },
+      (errors) => dispatch(receiveErrors('friendships', errors))
     )
 );
 
 export const updateFriendship = friendship => dispatch => (
   FriendUtil.updateFriendship(friendship)
-    .then(() => standardSuccess(dispatch),
-    (errors) => dispatch(receiveErrors('friendships', errors))
+    .then((success) => {
+        dispatch(clearErrors());
+        return dispatch(fetchAllFriendships());
+      },
+      (errors) => dispatch(receiveErrors('friendships', errors))
     )
 );
 
 export const destroyFriendship = id => dispatch => (
   FriendUtil.destroyFriendship(id)
-    .then(() => standardSuccess(dispatch),
-    (errors) => dispatch(receiveErrors('friendships', errors))
+    .then((success) => {
+        dispatch(clearErrors());
+        return dispatch(fetchAllFriendships());
+      },
+      (errors) => dispatch(receiveErrors('friendships', errors))
     )
 );
